4a358db5f3844976b1aaff9d7690507554b08b0a
legionjp <jjpsingh18@gmail.com>
2024-12-09 02:25:26 +0530

    Adding the While loop concept

---
+++ Conditional/looping.java
@@ -0,0 +1,21 @@
+package Conditional;
+
+public class looping {
+    public static void main(String a[])
+    {
+        //For Loop, # While Loop
+        int i = 1;
+        while(i<=5)
+        {
+            System.out.println("HI"+ i);
+            i++;
+            int j=1;
+            while (j<=3) {
+                System.out.println("Hello " +j);
+                j++;
+            }
+        }
+        System.out.println("Bye" + i); // at the time of being false
+
+    }
+}
