5e7111f2bed00cc0f3de915b112a33bd80b2bdda
legionjp <jjpsingh18@gmail.com>
2024-12-09 18:49:28 +0530

    Adding the multi dimenstion array

---
+++ Array/Array_MultiD.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class Array_MultiD {
+    public static void main(String a[])
+    {
+        System.out.println("This is Multi dimension Array ");
+
+        int nums[][] = new int[3][4];
+        
+
+    }
+}
+
+
+// Multi Dimension Array:
+/*
+ * n1 =[2,3,4,56,7] ---> 0[1]
+ * n2 = [ 3,4,5,6,7,8] ---> 1[1]
+ * n3 = [4[,5,6,7,8,0] ---> 2[1]
+ * N = [[n1],[n2],[n3]]  # Multi Dimensional array
+ * 
+ * Syntax: int nums [][] = new int[3][4];  ---> 3 array with the 4 elemnets
+ * 
+ */
