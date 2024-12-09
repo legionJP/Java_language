0a14464250673f6a8acd9a9250edeee20d9c2e30
= <=>
2024-12-09 12:00:13 +0530

    adding the for loop concept

--- Conditional/looping.java
+++ Conditional/looping.java
@@ -3,7 +3,7 @@ package Conditional;
 public class looping {
     public static void main(String a[])
     {
-        //For Loop, # While Loop
+        // # While Loop
         int i = 1;
         while(i<=5)
         {
@@ -17,5 +17,29 @@ public class looping {
         }
         System.out.println("Bye" + i); // at the time of being false
 
+    forloop f_Forloop = new forloop();
+    f_Forloop.floop();
     }
 }
+
+// For Loop
+
+class forloop{
+    void floop()
+    { 
+      for(int i=0;i<=3;i++)
+      {  
+      System.out.println("For Loop " +i);
+      }
+
+      for(int i=1;i<=7;i++)
+      {
+        System.out.println("Day "+i);        
+        for(int j=1;j<=9;j++)
+        {
+            System.out.println(" "+(j+8)+ "-" + (j+9));
+
+        }
+      }
+    }
+} 
