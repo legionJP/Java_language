84183235e41d15530aa39bafcc7a0b45e793e064
legionjp <jjpsingh18@gmail.com>
2024-12-15 01:25:46 +0530

    Adding the Polymorphism in java

---
+++ OOP_concept/Polymorphism/Polymorphism.java
@@ -0,0 +1,11 @@
+package OOP_concept.Polymorphism;
+
+public class Polymorphism {
+    
+    public static void main(String[] args) {
+        {
+            System.out.println(" Polymorphism in Java ");
+
+        }
+    }
+}
