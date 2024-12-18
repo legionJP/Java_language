d302d16e9b144945699d0fe7b3cbf7021587f46a
legionjp <jjpsingh18@gmail.com>
2024-12-19 00:32:57 +0530

    Adding the Abstract and anonymous class

---
+++ OOP_concept/AbstractAnonymous.java
@@ -0,0 +1,30 @@
+package OOP_concept;
+
+abstract class Abs{
+    public abstract void show();
+    public abstract void config();
+}
+
+
+public class AbstractAnonymous {
+    public static void main(String[] args)
+    {
+        Abs objAbs = new Abs() {
+            
+            public void show()
+            {
+                System.out.println(" This is Show ");
+            }
+
+            public void config()
+            {
+                System.out.println(" This is config ");
+            }
+        };
+        objAbs.config();
+        objAbs.show(); 
+    }
+}
+
+
+// Implementing the method for the abstract class and and method using the inner Class
