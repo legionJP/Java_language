b24a3dedf71fdd02a40f0fabbdff004958fd9363
legionjp <jjpsingh18@gmail.com>
2024-12-19 00:37:51 +0530

    Adding the Interface concept in Java

---
+++ Interface_Java/InterfaceJava.java
@@ -0,0 +1,16 @@
+package Interface_Java;
+
+
+
+
+
+public class InterfaceJava {
+    public static void main(String[] args) {
+        System.out.println(" Interface in Java ");
+    }
+}
+
+
+
+//  Need of the Interface
+
