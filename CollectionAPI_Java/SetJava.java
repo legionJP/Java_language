755f59a0ec1e9f955c5818f655284a43f0f05b37
legionjp <jjpsingh18@gmail.com>
2024-12-22 02:42:51 +0530

    Adding the Set in java Collection

---
+++ CollectionAPI_Java/SetJava.java
@@ -0,0 +1,29 @@
+package CollectionAPI_Java;
+
+public class SetJava {
+    public static void main(String[] args) {
+        System.out.println("Set in Java");
+
+      
+    }
+}
+
+
+
+  // Set is the interface in Java
+        // Set is the collection of the unique elements
+        // Set is the unordered collection of the elements
+        // Set is the interface so we can not create the object of the Set
+        // Set is implemented by the HashSet, LinkedHashSet, TreeSet
+
+        // HashSet: HashSet is the class which implements the Set interface
+        // HashSet is the collection of the unique elements
+        // HashSet is the unordered collection of the elements
+        // HashSet is the implementation of the hash table
+        // HashSet allows the null value
+        // HashSet does not maintain the insertion order
+        // HashSet is non-synchronized
+        // HashSet is not thread-safe
+        // HashSet is the best choice if our frequent operation is the search operation
+        // HashSet is the best choice if our frequent operation is the add and remove operation
+        // HashSet is the best choice if our frequent operation is the iteration
