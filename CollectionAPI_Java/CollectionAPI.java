987c6200929169e22d69007b0c45329db8483723
legionjp <jjpsingh18@gmail.com>
2024-12-22 02:21:28 +0530

    Adding the collection API

---
+++ CollectionAPI_Java/CollectionAPI.java
@@ -0,0 +1,33 @@
+package CollectionAPI_Java;
+
+public class CollectionAPI {
+    public static void main(String[] args) {
+        System.out.println("Collection API in Java");
+
+        
+}   
+}
+
+// Collection API : Collection API is a set of classes and interfaces that are used to store and manipulate the group of objects. 
+
+// collection : when taking about the collection it is interface which is used to store the group of the objects
+
+// colletions : when taking about the collections then it is class which is used to store the group of the objects
+
+// Collection API in Java
+        // Collection is a framework that provides an architecture to store and manipulate the group of objects.
+        // Java Collections can achieve all the operations that you perform on a data such as searching, sorting, insertion, manipulation, and deletion.
+        // Java Collection means a single unit of objects. Java Collection framework provides many interfaces (Set, List, Queue, Deque) and classes (ArrayList, Vector, LinkedList, PriorityQueue, HashSet, LinkedHashSet, TreeSet).
+        // Java Collection API provides a way to access prepackaged data structures as well as algorithms to manipulate them.
+        // Java Collection API contains interfaces, implementations, and algorithms.
+        // Java Collection API can be used to perform the following operations:
+        // Searching
+        // Sorting
+
+
+// int num =5;
+// int nums = new int[5];  // array of 5 elements 
+// when we need to use the lifo then we use the stack
+// when we need to use the fifo then we use the queue 
+
+//
