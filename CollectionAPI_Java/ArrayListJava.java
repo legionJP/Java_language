2b2c4c99467e25b8f8b91057a884210f26d38885
legionjp <jjpsingh18@gmail.com>
2024-12-26 21:49:06 +0530

    Updating the array list

--- CollectionAPI_Java/ArrayListJava.java
+++ CollectionAPI_Java/ArrayListJava.java
@@ -1,5 +1,7 @@
 package CollectionAPI_Java;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ArrayListJava {
@@ -7,29 +9,56 @@ public class ArrayListJava {
         System.out.println("ArrayList in Java");
 
         // Collection API  belongs to the java.util package
-        // Collection API is a framework that provides the architecture to store and manipulate the group of objects
+// Collection API is a framework that provides the architecture to store and manipulate the group of objects
 
-        Collection <Integer> nums = new ArrayList<Integer>();  // using the generic type to store the integer values
-        //List <Integer> nums = new ArrayList<Integer>();  // using the List interface to store the integer values
+        Collection nums0 = new ArrayList();
+        nums0.add(1);
+        nums0.add(2);
+        nums0.add(3);
+        System.out.println(nums0);
+
+// using the generic type to store the integer values
+
+         Collection <Integer> nums = new ArrayList<Integer>();  // angular bracket for mention the type of the object 
+         Collection <String> nums1 = new ArrayDeque<String>();
+
+// using the List interface to store the integer values
+
+        //List <Integer> nums = new ArrayList<Integer>(); 
+
         nums.add(6);
         nums.add(7);
         nums.add(8);
         nums.add(9);
 
         // System.out.println(nums);
-        // System.out.println(nums.indexOf(6));  // it will give the index of the element
+        // System.out.println(nums.indexOf(6));  // it will give the index of the element bcz list support the index value
         // System.out.println(nums.contains(7));  // it will give the boolean value if the element is present or not
         // System.out.println(nums.get(2));  // it will give the element at the index 2
 
-        for (int i : nums) {
+        System.out.println(nums); // print directly the collection array list
+
+        for (int i : nums) { 
             System.out.println(i);
+
+// Typecasting the int to integer 
+            // int num = (Integer)i;
+            // System.out.println(num*2);
+
         }
 
     }
 
-    // ArrayList is a class which uses the List interface to store the elements
-    // If you want to use the index  use the List instead of the Collection
+   
 }
+
+//Array List
+
+ // ArrayList is a class which uses the List interface to store the elements
+// If you want to use the index  use the List instead of the Collection
+
+
+
 
 // Collection is implented by the List, Set, Queue, Deque
 // List is implemented by the ArrayList, LinkedList, Vector, Stack
