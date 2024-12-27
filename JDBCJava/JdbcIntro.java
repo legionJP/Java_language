46d08cd772ed6f6eff2c1224ad32baf9fc855667
legionjp <jjpsingh18@gmail.com>
2024-12-28 02:04:18 +0530

    Adding the JDBC intro

---
+++ JDBCJava/JdbcIntro.java
@@ -0,0 +1,17 @@
+package JDBCJava;
+
+public class JdbcIntro {
+    public static void main(String[] args) {
+        System.out.println(" JDBC ");
+
+    }
+}
+
+// JDBC : java Database Connectivity 
+// Need to use the SQL to store the data 
+// cooncetion b/w the application ---> JDBC , it is a API part of JDK 
+
+// Actual implementation is done by the databases or dbms 
+// we need jar file to coonect it 
+
+// Postgres Setup
