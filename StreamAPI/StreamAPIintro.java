08f95e87d77a02ee02ca037836feed9e55b80a2f
legionjp <jjpsingh18@gmail.com>
2024-12-27 16:29:45 +0530

    Update

--- StreamAPI/StreamAPIintro.java
+++ StreamAPI/StreamAPIintro.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class StreamAPIJava {
+public class StreamAPIintro {
     public static void main(String[] args) {
 
         // List<Integer> nums = new ArrayList<>();
