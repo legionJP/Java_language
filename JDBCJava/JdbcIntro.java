acdb54670142040d4d08a3d3f86a4825d624431d
legionjp <jjpsingh18@gmail.com>
2024-12-28 14:24:47 +0530

    Adding the Postgres intallation and setup instructions

--- JDBCJava/JdbcIntro.java
+++ JDBCJava/JdbcIntro.java
@@ -14,4 +14,35 @@ public class JdbcIntro {
 // Actual implementation is done by the databases or dbms 
 // we need jar file to coonect it 
 
-// Postgres Setup
+// Postgres Setup in Linux Debian based
+// Install
+// sudo apt update
+// sudo apt install postgresql postgresql-contrib
+
+// Enable 
+
+// sudo systemctl enable postgresql
+// sudo systemctl start postgresql
+
+// Access database
+// sudo -u postgres psql  // as the super user 
+
+// Create a New Database and User
+/*
+ * CREATE DATABASE mydatabase;
+   CREATE USER myuser WITH ENCRYPTED PASSWORD 'mypassword';
+   GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+   psql -U myuser -d mydatabase
+
+ * 
+ * // Change the auth method to login usign the password 
+ * 
+ * sudo nano /etc/postgresql/Version_name/main/pg_hba.conf
+ * change from peer to md5 for the local user 
+ * local   all             all                                     md5
+ * sudo systemctl restart postgresql
+ * 
+ */
+
+ // connect new user with the databse using the md5 
+//    psql -U myuser -d mydatabase
