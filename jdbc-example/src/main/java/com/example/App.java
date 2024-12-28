7712d99fc5e14b9091f466d840eb957466e9d1ff
legionjp <jjpsingh18@gmail.com>
2024-12-29 01:47:47 +0530

    Adding the db execute adn rcords fetching

--- jdbc-example/src/main/java/com/example/App.java
+++ jdbc-example/src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 
 import org.json.simple.JSONObject;
@@ -15,7 +19,7 @@ public class App
         // System.out.println( "Hello World!" );
 
         // load and register
-        Class.forName("org.postgresql.Driver");
+        Class.forName("org.postgresql.Driver"); // this is optional 
 
         // Read the configuration file: 
         JSONParser parser = new JSONParser();
@@ -28,13 +32,38 @@ public class App
         String pass = (String) config.get("password");
 
         // OR
-        
+
         // // create the connection // Connection is interface so DriverManager is utility and has the getconnection 
         // String url = "jdbc:postgresql://localhost:5432/jdatabase";
         // String uname = "user";
         // String pass = "password";
         Connection con = DriverManager.getConnection(url,uname,pass);
         System.out.println("Connection is Successful");
+
+//  Getting the data from database;
+        String query = "select sname from student where sid=2";
+
+        Statement  st = con.createStatement(); // it is interface
+
+// execute statement
+        //st.executeQuery(query);  // geting the quey 
+
+        ResultSet rs = st.executeQuery(query);
+       // System.out.println(rs.next());  // gives the bool value if have the next row return true 
+
+// printing the data 
+
+        //use the .next after executing the query as .next get the 1st record if you are before the 1st line
+        rs.next();
+        String name=  rs.getString("sname");  // need to mention the columnname 
+        System.out.println("Name of the student is "+ name);
+        rs.getString(1); //getting the column number to get columnn data
+
+
+// Fetching all the records 
+        
+
+        con.close();
 
         // connection url is the databse number 
 
