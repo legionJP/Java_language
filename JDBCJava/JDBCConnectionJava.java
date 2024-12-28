0e627a66b45f14fb3a1e3493c196fb9b59393deb
legionjp <jjpsingh18@gmail.com>
2024-12-28 18:52:25 +0530

    restructuring the dir and adding the jdbc conncetor example

---
+++ JDBCJava/JDBCConnectionJava.java
@@ -0,0 +1,29 @@
+package JDBCJava;
+
+public class JDBCConnectionJava {
+    public static void main(String[] args) {
+        System.out.println(" JDBC Steps ");
+    }
+}
+
+
+
+
+
+
+// Steps for the JDBC
+
+/*
+ * 1.  Get the jar File 
+ * | Steps --_|
+ *
+ * 2. Import the Packages ---> Java.sql
+ * 3. Load the Driver which is coming from jar file 
+ * 4. Register the Driver 
+ * 5. Create the Connection Object
+ * 6. Create the Statement 
+ * 7. Execute the Statement 
+ * 8. Close the connection to prevent the leaking the resources. 
+ * 
+ * 
+ */
